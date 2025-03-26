5c7ac8556163796689d0a67870d9130532704591
Kentonmk <kenton.gobin@csuglobal.edu>
2025-03-26 00:09:25 -0600

    Add files via upload

---
+++ PersonalInfo.java
@@ -0,0 +1,15 @@
+public class PersonalInfo {
+    public static void main(String[] args) {
+        String firstName = "Mark";
+        String lastName = "Thomas";
+        String streetAddress = "123 Second Street";
+        String city = "Brighton";
+        String zipCode = "80602";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
